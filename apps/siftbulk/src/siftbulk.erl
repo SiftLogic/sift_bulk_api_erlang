a36997bc3035004cacb53ee6268662c251a6b493
Jacob Friesen <jacob@jacobfriesen.com>
2014-06-18 12:26:21 -0400

    (feat) SiftLogic/api_sample_libraries#10 Added type specifications for siftbulk

--- apps/siftbulk/src/siftbulk.erl
+++ apps/siftbulk/src/siftbulk.erl
@@ -7,14 +7,20 @@
 -export([start_link/0, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2]).
 -export([set_opts/1, get_opts/0, init/1, stop/0]).
 
+-type connection_part() :: {atom(), any()}.
+-type connection_list() :: list(connection_part()).
+-type state() :: #state{}.
+
 %% Private
 
+-spec set_opts(state(), connection_list()) -> state().
 set_opts(#state{} = State, []) ->
     State;
 set_opts(#state{} = State, Opts) ->
     Fields = [host, password, poll_every, port, username],
     set_opts(State, Fields, Opts).
 
+-spec set_opts(state(), list(), connection_list()) -> state().
 set_opts(#state{} = State, [], _Opts) ->
     State;
 set_opts(#state{opts = OldOpts} = State, [Field | Tail], Opts) ->
@@ -36,6 +42,7 @@ set_opts(#state{opts = OldOpts} = State, [Field | Tail], Opts) ->
 
 %% Starts the API server.
 
+-spec start_link() -> {atom(), pid()}.
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
@@ -46,6 +53,7 @@ start_link() ->
 %% port: The port to connect to. Defaults to 21.
 %% poll_every: Number of seconds to poll. Defaults to 300 (5 minutes)if falsey.
 
+-spec init([]) -> {ok, state()}.
 init([]) ->
     Opts = case application:get_env(siftbulk, auth) of % Get application configuration variables
                {ok, Opts0} ->
@@ -59,6 +67,12 @@ init([]) ->
 %% Handles all synchronous calls to the server. See specific functions for more
 %% detailed information.
 
+-spec handle_call({set_opts, connection_list()}, any(), any()) ->
+                     {reply, ok, state()};
+                 (get_opts, any(), state()) -> 
+                     {reply, connection_list(), state()};
+                 (stop, any(), any()) -> 
+                     {stop, normal, ok, any()}.
 handle_call({set_opts, Opts}, _From, State) ->
     NewState = set_opts(State, Opts),
     {reply, ok, NewState};
@@ -72,34 +86,39 @@ handle_call(_Msg, _From, State) ->
 %% Handles all asynchronous calls to the server. See specific functions for more
 %% detailed information.
 
+-spec handle_cast(any(), any()) -> {noreply, any()}.
 handle_cast(_Msg, State) ->
     {noreply, State}.
  
 %% Handles direct messages to this server.
 
+-spec handle_info(any(), any()) -> {noreply, any()}.
 handle_info(_Msg, State) ->
     {noreply, State}.
 
 %% Handles hot code upgrades. Currently, does nothing.
 
+-spec code_change(any(), any(), any()) -> {ok, any()}.
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
  
 %% Handles stopping of the server. Currently, just returns nothing.
 
+-spec terminate(any(), any()) -> ok.
 terminate(_Reason, _State) ->
     ok.
 
 % Stops this server
 
+-spec stop() -> ok.
 stop() ->
     gen_server:call(?MODULE, stop).
 
 %% Allows updating of the state by specifying a list of tuples or a single tuple
 %% e.g. set_opts([username, <<"TestUser">>]).
 
--spec set_opts(list() | tuple()) -> ok | {error, badarg} |
-               {error, badarg, binary()}.
+-spec set_opts(connection_list()) -> ok | {error, badarg, binary()};
+              (connection_part()) -> ok | {error, badarg, binary()}.
 set_opts(Opts) when is_list(Opts) ->
     gen_server:call(?MODULE, {set_opts, Opts});
 set_opts(OptTuple) when is_tuple(OptTuple) ->
@@ -108,5 +127,6 @@ set_opts(OptTuple) when is_tuple(OptTuple) ->
 %% Returns the connections details. Namely, username, password, host, port and
 %% poll_every.
 
+-spec get_opts() -> connection_list().
 get_opts() ->
     gen_server:call(?MODULE, get_opts).
